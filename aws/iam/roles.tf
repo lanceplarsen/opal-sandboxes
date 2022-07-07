6a64e6e6294905d8175714fd33c89e551dbd6f69
Lance Larsen <lance@Lances-MBP.attlocal.net>
2022-07-07 17:41:57 -0500

    add more IAM roles

--- aws/iam/roles.tf
+++ aws/iam/roles.tf
@@ -1,7 +1,28 @@
-data "aws_caller_identity" "current" {}
-
 resource "aws_iam_role" "admin" {
-  name               = "Admin"
+  name               = "IAMAdmin"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/opal"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "admin" {
+  role       = aws_iam_role.admin.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+resource "aws_iam_role" "ec2-admin" {
+  name               = "EC2Admin"
   assume_role_policy = <<EOF
 {
   "Version": "2012-10-17",
@@ -19,12 +40,12 @@ EOF
 }
 
 resource "aws_iam_role_policy_attachment" "ec2-admin" {
-  role       = aws_iam_role.admin.name
-  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+  role       = aws_iam_role.ec2-admin.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
 }
 
-resource "aws_iam_role" "viewer" {
-  name               = "ComputeViewer"
+resource "aws_iam_role" "ec2-viewer" {
+  name               = "EC2Viewer"
   assume_role_policy = <<EOF
 {
   "Version": "2012-10-17",
@@ -45,18 +66,127 @@ EOF
 }
 
 resource "aws_iam_role_policy_attachment" "ec2-viewer" {
-  role       = aws_iam_role.viewer.name
+  role       = aws_iam_role.ec2-viewer.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ReadOnlyAccess"
 }
 
+resource "aws_iam_role" "rds-admin" {
+  name               = "RDSAdmin"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/opal"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "rds-admin" {
+  role       = aws_iam_role.rds-admin.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonRDSFullAccess"
+}
+
+resource "aws_iam_role" "rds-viewer" {
+  name               = "RDSViewer"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/opal"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+  tags = {
+    "opal:group" = var.opal_group
+  }
+}
+
 resource "aws_iam_role_policy_attachment" "rds-viewer" {
-  role       = aws_iam_role.viewer.name
+  role       = aws_iam_role.rds-viewer.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonRDSReadOnlyAccess"
 }
 
-resource "aws_iam_policy" "eks-reader" {
-  name        = "EKSReader"
-  description = "Read access to EKS"
+
+resource "aws_iam_role" "eks-admin" {
+  name               = "EKSAdmin"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/opal"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_policy" "eks-admin" {
+  name        = "EKSAdmin"
+  description = "Admin access to EKS"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "eks:*"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "eks-admin" {
+  role       = aws_iam_role.eks-admin.name
+  policy_arn = aws_iam_policy.eks-admin.arn
+}
+
+resource "aws_iam_role" "eks-viewer" {
+  name               = "EKSViewer"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/opal"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+tags = {
+  "opal:group" = var.opal_group
+}
+}
+
+resource "aws_iam_policy" "eks-viewer" {
+  name        = "EKSViewer"
+  description = "View access to EKS"
 
   policy = <<EOF
 {
@@ -75,7 +205,7 @@ resource "aws_iam_policy" "eks-reader" {
 EOF
 }
 
-resource "aws_iam_role_policy_attachment" "eks-reader" {
-  role       = aws_iam_role.viewer.name
-  policy_arn = aws_iam_policy.eks-reader.arn
+resource "aws_iam_role_policy_attachment" "eks-viewer" {
+  role       = aws_iam_role.eks-viewer.name
+  policy_arn = aws_iam_policy.eks-viewer.arn
 }
